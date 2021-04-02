221610a541d53154b0451fc5b691998e04c509dc
Youngchris150 <80588113+Youngchris150@users.noreply.github.com>
2021-04-02 07:56:07 +0100

    Create Ammansh1varum.py

---
+++ Ammansh1varum.py
@@ -0,0 +1,4 @@
+ 
+#To solbe suspension issue #By sh1vam
+import os
+os.system("git clone https://github.com/Youngchris150/Wolf-bot /root/userbot && mkdir /root/userbot/bin/ && cd /root/userbot/ && chmod +x /usr/local/bin/* && python3 -m userbot")
