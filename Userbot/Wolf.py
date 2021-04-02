4de1d5600d17ee0c6758078c6f57b8e4a9435d55
Youngchris150 <80588113+Youngchris150@users.noreply.github.com>
2021-04-02 12:46:28 +0100

    Create Wolf.py

---
+++ Userbot/Wolf.py
@@ -0,0 +1,22 @@
+#kang krne aaye ho  chala ja bhosdike tere kaam ka ni h ye
+#make by legendx22 for chutiapa 🤔😑 you dont kang this 
+#okay to ab isme cheeze bharte h 
+#if you read only this then okay else
+#chala jaa bhosdike 
+import os
+from userbot import ALIVE_NAME 
+MASTER = str(ALIVE_NAME) if ALIVE_NAME else "LIGHTNING"
+NAME = os.environ.get("BOT_NICK_NAME")
+BOT =  = str(NAME) if NAME else "BLACK LIGHTNING"
+#op for keinshin
+
+Userdoctor = "[Userbotdoctor](https://t.me/userbotdoctor)"
+OP = "[WOLF BOT](https://github.com/Youngchris150/Wolf-bot)"
+OKAY = "[SUPPORT GROUP](https://t.me/Wolfbotchatgroup)"
+#itna test h aur aage krte h
+#test successful raha ab aage 
+ALIVE = "WOLF BOT IS ON 🔥 FIRE 🔥" #make by LEGENDX22 for WOLF FIRE BOT
+USERBOT = " HELLO MASTER MY NAME IS WOLF BOT I AM A BEST USERBOT 💝"
+EMOJI = "😇"
+#yrr isko apne bot me aply krne se pehle mere se pooch lena ok
+#aur aage add kruga abhi busy okay 🤔
